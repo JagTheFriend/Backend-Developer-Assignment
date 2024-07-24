76a9f6a0e3fdb339a4a9fb25fa9217f82d4623c5
JagTheFriend <JagTheFriend12@gmail.com>
2024-07-24 17:59:07 +0530

    Added documentation
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- assignment/database.py
+++ assignment/database.py
@@ -5,7 +5,22 @@ db = SQLAlchemy()
 
 
 class BookingsTable(db.Model):
-    booking_id: Mapped[int] = mapped_column(primary_key=True)
+    """
+    Attributes:
+        booking_id (int): The unique ID of the booking.
+        user_id (int): The ID of the user who made the booking. This is marked as unique to
+            make sure that a retreat cannot be double-booked for the same user.
+        user_name (str): The name of the user who made the booking.
+        user_email (str): The email of the user who made the booking.
+        user_phone (int): The phone number of the user who made the booking.
+        retreat_id (int): The ID of the retreat that was booked. This is set as the primary key.
+        retreat (RetreatTable): A relationship to the RetreatTable model that represents the retreat
+            that was booked.
+        payment_details (str): The details of the payment made for the booking.
+        booking_date (int): The date of the booking.
+    """
+
+    id: Mapped[int] = mapped_column(primary_key=True)
 
     # Mark user Id as unique to make sure retreat cannot be double-booked for the same user.
     user_id: Mapped[int] = mapped_column(unique=True)
@@ -22,6 +37,21 @@ class BookingsTable(db.Model):
 
 
 class RetreatTable(db.Model):
+    """
+    Attributes:
+        id (int): The unique ID of the retreat.
+        title (str): The title of the retreat.
+        description (str): The description of the retreat.
+        date (int): The date of the retreat.
+        location (str): The location of the retreat.
+        price (int): The price of the retreat.
+        type (str): The type of the retreat.
+        condition (str): The condition of the retreat.
+        image (str): The image of the retreat.
+        tag (str): The tag of the retreat.
+        duration (int): The duration of the retreat.
+    """
+
     id: Mapped[int] = mapped_column(primary_key=True)
     title: Mapped[str] = mapped_column()
     description: Mapped[str] = mapped_column()
