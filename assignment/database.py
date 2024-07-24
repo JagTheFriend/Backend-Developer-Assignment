c4d94453722018965a2baeaf33ae2431111a157f
JagTheFriend <JagTheFriend12@gmail.com>
2024-07-24 16:40:30 +0530

    Added another model to save all the bookings
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- assignment/database.py
+++ assignment/database.py
@@ -18,3 +18,22 @@ class RetreatTable(db.Model):
 
     tag: Mapped[str] = mapped_column()
     duration: Mapped[int] = mapped_column()
+
+
+class BookingsTable(db.Model):
+    # Mark user Id has unique to make sure retreat cannot be double-booked for the same user.
+    user_id: Mapped[int] = mapped_column(unique=True)
+    user_name: Mapped[str] = mapped_column()
+    user_email: Mapped[str] = mapped_column()
+    user_phone: Mapped[int] = mapped_column()
+
+    # Set retreat_id as the primary key
+    retreat_id: Mapped[int] = mapped_column(primary_key=True)
+    retreat_title: Mapped[str] = mapped_column()
+    retreat_location: Mapped[str] = mapped_column()
+
+    retreat_price: Mapped[int] = mapped_column()
+    retreat_duration: Mapped[int] = mapped_column()
+
+    payment_details: Mapped[str] = mapped_column()
+    booking_date: Mapped[int] = mapped_column()
