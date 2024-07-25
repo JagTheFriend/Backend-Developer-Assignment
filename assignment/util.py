21ab0dd7c624079f8f9ded4d2cc0e6e569a583cb
JagTheFriend <JagTheFriend12@gmail.com>
2024-07-25 10:41:23 +0530

    Added doc string
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- assignment/util.py
+++ assignment/util.py
@@ -3,10 +3,19 @@ from database import RetreatTable
 from json import loads
 
 
-def extract_query_content(results: Sequence[RetreatTable]):
-    data: list[RetreatTable] = []
-    for result in results:
-        data.append(
+def extract_query_results(query_results: Sequence[RetreatTable]) -> list[dict]:
+    """
+    Extracts the relevant information from the query results and returns a list of extracted data.s
+
+    Args:
+        query_results (Sequence[RetreatTable]): The results of a SQL query.
+
+    Returns:
+        list[dict]: A list of dictionaries containing the extracted information.
+    """
+    extracted_data: list[dict] = []
+    for result in query_results:
+        extracted_data.append(
             {
                 "title": result.title,
                 "description": result.description,
@@ -21,4 +30,4 @@ def extract_query_content(results: Sequence[RetreatTable]):
                 "id": result.id,
             }
         )
-    return data
+    return extracted_data
